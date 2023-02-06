57cc3ab6fecb8c5febd5fab02313fdba40e5d28c
ISHIKA-CODESQUARE <ishika.joshi@codesquaretech.com>
2023-02-06 16:37:58 +0530

    test changes

--- FriendListModule/cartridges/app_custom_GiftCardFunctionality/cartridge/models/productLineItem/orderLineItem.js
+++ FriendListModule/cartridges/app_custom_GiftCardFunctionality/cartridge/models/productLineItem/orderLineItem.js
@@ -5,7 +5,7 @@ var base =  module.superModule;
 module.exports = function orderLineItem(product, apiProduct, options) {
     base(product, apiProduct, options);
     product.isGiftCard = apiProduct.custom.isGiftCard; // update attribute for order of giftcard
-    product.SenderID = options.lineItem.custom.senderID;
+    // product.SenderID = options.lineItem.custom.senderID;
 
     return product
 }
