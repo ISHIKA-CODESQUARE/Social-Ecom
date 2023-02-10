e6369e0678e0e0a266c0750298c015b3bef397ad
ISHIKA-CODESQUARE <ishika.joshi@codesquaretech.com>
2023-02-10 14:26:27 +0530

    gift card grand total price

--- FriendListModule/cartridges/int_ecomSocial/cartridge/models/order.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-function OrderModel(lineItemContainer, options) {
-    module.superModule.call(this,lineItemContainer, options);
-    // adding gift card attribute for calculating gift card total - CUSTOM
-       this.giftCardTotal = lineItemContainer.giftCertificateTotalPrice ? lineItemContainer.giftCertificateTotalPrice.value :null;
-}
-
-module.exports = OrderModel;
